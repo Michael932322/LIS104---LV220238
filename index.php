d94a4404e848fc6c3528b302648d7c6058905d4a
LV220238 <Michael.Alejandro.Lopez.Valle@gmail.com>
2025-02-03 10:57:29 -0600

    Arreglos

--- index.php
+++ index.php
@@ -1,27 +1,12 @@
 <?php
 $projects = [
-    'Ejemplo 1 - Guia 2' => 'Ejemplo_1\bisiesto.php',
-    'Ejemplo 2 - Guia 2' => 'Ejemplo_2\Salario.php',
-    'Ejemplo 3 - Guia 2' => 'Ejemplo_3\Compararnumeros.html',
-    'Ejemplo 4 - Guia 2' => 'Ejemplo_4\Convertir.php',
-    'Ejemplo 5 - Guia 2' => 'Ejemplo_5\Convmonedas.php',
-    'Resultados - Guia 2' => 'Resultados_2\Calculo_dias.php'
+    'Ejemplo 1 - Guia 2' => 'Ejemplo_1.2\bisiesto.php',
+    'Ejemplo 2 - Guia 2' => 'Ejemplo_2.2\Salario.php',
+    'Ejemplo 3 - Guia 2' => 'Ejemplo_3.2\Compararnumeros.html',
+    'Ejemplo 4 - Guia 2' => 'Ejemplo_4.2\Convertir.php',
+    'Ejemplo 5 - Guia 2' => 'Ejemplo_5.2\Convmonedas.php',
+    'Resultados - Guia 2' => 'Resultados_2.2\Calculo_dias.php'
 ];
-/*<button onclick="window.location.href='../index.php'" style="
-        padding: 10px 20px;
-        font-size: 16px;
-        background-color: #007bff;
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: background-color 0.3s;
-        position: fixed;
-        bottom: 0;
-        left: 50%;
-        transform: translateX(-50%);
-        z-index: 1000;
-    ">Regresar</button> */
 ?>
 
 <!DOCTYPE html>
